110fb8257edc662bc3b81b18067de1d6226ea8d3
Andrey Glumov <gluman@yandex.ru>
2025-02-24 00:08:33 +0300

    final commit today

---
+++ task.js
@@ -0,0 +1,11 @@
+const input_text = document.querySelector('input')
+const input_btn = document.querySelector('button')
+let task_list = document.getElementsByClassName('task__list')
+console.log(input_text)
+let task_element = null
+input_btn.addEventListener('click', (event)=> {
+    event.preventDefault()
+    if(input_text.textContent != null) {
+        
+    }
+})
